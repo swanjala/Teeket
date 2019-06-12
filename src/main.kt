664c585740be0f3002459de0b38ae92663490590
swanjala <wanjala.sam.s@gmail.com>
2019-06-12 19:16:27 +0300

    (add): greeting & instructions

--- src/main.kt
+++ src/main.kt
@@ -2,4 +2,18 @@ import models.Passenger
 
 fun main(args:Array<String>){
 
+
+    print("${greetingText()}")
+
+
+}
+
+fun greetingText() : String{
+    val greetingInstructions:String = "\t WELCOME TO Teeket\n" +
+            "Choose an option below : \n" +
+            "\t\t1. Book a ticket \n" +
+            "\t\t2. List all trip to a destination"
+
+
+    return greetingInstructions
 }
