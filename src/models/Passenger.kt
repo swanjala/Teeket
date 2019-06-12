b6e0dcb6b16fcaec788826630bdd6e11b7797f8b
swanjala <wanjala.sam.s@gmail.com>
2019-06-12 14:35:23 +0300

    (add): data classes

---
+++ src/models/Passenger.kt
@@ -0,0 +1,3 @@
+package models
+
+data class Passenger (var passengerId:Int, var name:String, var age:Int, var travelClass:String)
---
+++ src/models/Station.kt
@@ -0,0 +1,7 @@
+package models
+
+/* learn how to implemnt pojos(they may actually not be names pojos anymore)
+ because this is not java in Kotlin*/
+
+
+
