8a042e8ef1a597f67afbf8ec1b1264713489bbd1
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 17:32:26 +0300

    (ref): remove dead code

--- src/main.kt
+++ src/main.kt
@@ -2,45 +2,4 @@ import models.Passenger
 
 fun main(args:Array<String>){
 
-
-    print("${greetingText()}")
-
-
-    val option:Int = readLine()!!.toInt()
-
-    while (option != 3) {
-
-        if (option == 1) {
-            bookTicket()
-        } else if (option == 2) {
-            listTripsToDestination()
-        }
-        print(option)
-    }
-
-
 }
-
-fun greetingText() : String{
-    val greetingInstructions:String = "\t WELCOME TO Teeket\n" +
-            "Choose an option below : \n" +
-            "\t\t1. Book a ticket \n" +
-            "\t\t2. List all trip to a destination\n" +
-            "\t\t3. Exit program"
-
-
-    return greetingInstructions
-}
-
-fun bookTicket(){
-    print("Welcome to Ticket Booking")
-}
-
-fun invalidateTrip(){
-
-}
-
-fun listTripsToDestination() {
-    print("Welcome to List trips")
-}
-
