ba8fbfb14f1c223fb943bee393a918932b1a961e
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 18:35:21 +0300

    (add): passanger data class
    - Data declarations
    - Implement custom setters and getters

--- src/models/Passenger.kt
+++ src/models/Passenger.kt
@@ -1,11 +1,35 @@
 package models
 
-data class Passenger(
-    var passengerId: Int,
-    var name: String,
-    var age: Int,
-    var passengerType: String,
-    var passengerTravelStatus: String,
-    var passengerTravelClass: String,
-    var travelClass: String
-)
+import java.lang.IllegalArgumentException
+
+class Passenger{
+
+    var passangerId:Int = 0
+
+    set(value){
+        field = if(passangerId is Int) value else
+            throw IllegalArgumentException("Passanger Id must be Integer")
+    }
+    var name :String = ""
+    get() {
+        return name.toLowerCase().trim()
+    }
+    var age:Int = 0
+
+    set(value){
+        field = if(age is Int && age >17) value else
+            throw IllegalArgumentException("Age should be int and above 17")
+    }
+    var passengerType:String = ""
+    get() {
+        return passengerType.toLowerCase()
+    }
+    var passengerTravelStatus:String = ""
+    var passengerTravelClass:Int = 0
+    set(value){
+        field = if(passengerTravelClass is Int && passengerTravelClass >4 )value
+        else throw IllegalArgumentException("Travel class should be int with 3 options")
+    }
+
+    var travelClass: String = ""
+}
