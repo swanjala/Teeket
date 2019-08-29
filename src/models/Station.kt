1ae620de65dfa29abf45b877304df239a4bb8fa0
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 18:41:45 +0300

    (add): station data class
    - Variable declaration
    - Custom setters and getters for validation checks

--- src/models/Station.kt
+++ src/models/Station.kt
@@ -1,9 +1,29 @@
 package models
 
-data class Station(
-    val stationId: Int,
-    val stationName: String,
-    val stationState: String,
-    val stationCounty: String,
-    val stationCapacity: Int
-)
+import java.lang.IllegalArgumentException
+
+class Station{
+
+    var stationId:Int = 0
+    set(value){
+        field = if (stationId is Int) value else throw
+                IllegalArgumentException("Station Id should be Integer")
+    }
+    var stationName:String = ""
+    get() {
+        return stationName.trim().toLowerCase()
+    }
+    var stationState: String = ""
+        get() {
+            return stationState.trim().toLowerCase()
+        }
+    var stationCounty: String = ""
+        get() {
+            return stationCounty.trim().toLowerCase()
+        }
+    var stationCapacity:Int = 0
+        set(value){
+            field = if (stationCapacity is Int) value else throw
+            IllegalArgumentException("Station Capacity should be Integer")
+        }
+}
