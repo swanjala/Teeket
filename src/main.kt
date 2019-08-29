82c23d9ed386f71358be140bd6e6421a36902ee0
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 18:11:38 +0300

    (add): bus data model
    - Declare necessary varialbes
    - Implement custom getter for bus name
    - Impelement custom setters with illegal argument exceptions

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,13 @@
+import models.Bus
 import models.Passenger
 
 fun main(args:Array<String>){
+
+}
+fun dataInit() {
+    var busData = Bus()
+
+    busData.busName = "MyBus"
+    busData.busCapacity = 12
 
 }
--- src/models/Bus.kt
+++ src/models/Bus.kt
@@ -1,9 +1,31 @@
 package models
 
-data class Bus(
-    var busId: Int,
-    var busName: String,
-    var busCapacity: Int,
-    var busLaggageCapacity: Int,
-    var busRouteCode: Int
-)
+import java.lang.IllegalArgumentException
+
+class Bus{
+
+    var busName: String = ""
+    get() {
+        return field.toLowerCase()
+    }
+    set(value){
+        field = busName
+    }
+
+    var busCapacity:Int = 0
+
+    set(value){
+        field= if (value < 0) value else
+            throw IllegalArgumentException("Bus cannot have negative capacity")
+    }
+
+    var luggageCapacity:Int = 0
+
+    set(value){
+        field = if (value < 0 ) value else
+            throw IllegalArgumentException("Luggage capacity cannot be negative")
+    }
+
+    var routeCode: Int = 0
+
+}
