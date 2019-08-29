90a55fe2889727fd327dec4ecfd6522323821b30
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 18:23:56 +0300

    (add) Driver data class
    - Declare driver variables
    - Implement custome setters and getters for
    driver age, gender and type

--- src/models/Driver.kt
+++ src/models/Driver.kt
@@ -1,9 +1,32 @@
 package models
 
-data class Driver(
-    var driverId:Int,
-    var driverName:String,
-    var driverAge:Int,
-    var driverGender:Int,
-    var driverType:String
-)
+import java.lang.IllegalArgumentException
+
+class Driver{
+    var driverId:Int = 0
+
+    set(value){
+        field = if (value is Int) value else
+            throw IllegalArgumentException ("Value must be an integer")
+    }
+
+    var driverName:String = ""
+    get(){
+        return field.toLowerCase()
+    }
+    var driverAge:Int  = 0
+    set(value){
+        field = if (value < 18) value else
+            throw IllegalArgumentException("Driver is  underage")
+    }
+    var driverGender: String = ""
+
+    get(){
+        return field.toLowerCase()
+    }
+    var driverType: String = ""
+    get(){
+        return field.toLowerCase()
+    }
+}
+
