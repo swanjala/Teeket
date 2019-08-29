a10155d5cc22c6254c583eb46e41057efc332847
live-beta <wanjala.sam.s@gmail.com>
2019-08-29 18:56:29 +0300

    (add): trip data class
    - Implement the trip data class
    declarations
    - Add custom setters and getters

--- src/models/Trip.kt
+++ src/models/Trip.kt
@@ -1,15 +1,39 @@
 package models
 
+import java.lang.IllegalArgumentException
 import java.util.Date
 
-data class Trip(
-    var tripState: String,
-    var tripStartTime: Date,
-    var tripExpectedArrivalTime: Date,
-    var startPoint: String,
-    var destination: String,
-    var stations: ArrayList<Station>,
-    var bus: String,
-    var passengers: ArrayList<Passenger>,
-    var driver: String
-)
+class Trip {
+    var tripState:String = ""
+    get(){
+        return tripState.trim().toLowerCase()
+    }
+    var tripStartTime: Date? = null
+
+    set(value){
+        field = if(value is Date)value else throw
+                IllegalArgumentException(" Start time should be type date")
+    }
+    var tripExpectedArrivalTime:Date? = null
+
+    set(value){
+        field = if(value is Date)value else throw
+                IllegalArgumentException("Expected time of arrival should be " +
+                        "type date")
+    }
+    var startPoint:String = ""
+    get() {
+        return startPoint.trim().toLowerCase()
+    }
+    var destination:String = ""
+    var stations:ArrayList<Station>? = null
+    set(value){
+        field = if(stations is ArrayList)value else throw
+                IllegalArgumentException("The data expects arraylist")
+    }
+    var busId:Int? = null
+    set(value){
+        field = if(value is Int)value else throw
+                IllegalArgumentException("BusId should be an integer")
+    }
+}
