0f03dcacc3b93700ac51930cfd92a7a74261d324
live-beta <wanjala.sam.s@gmail.com>
2019-08-31 04:37:51 +0300

    (test): Add unit tests for registration
    - Test that data can be added and removed successfully

--- src/test/RegistrationTest.kt
+++ src/test/RegistrationTest.kt
@@ -1,11 +1,43 @@
 package test
 
+import models.Passenger
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class RegistrationTest {
 
-    @Test
-    fun test_data_is_added_successfully(){
+    companion object{
 
+        @JvmStatic
+        var passanger:Passenger = Passenger()
+        var dataCountInit:Int = 0
+        var dataCountFinal:Int = 0
+        var personalData:ArrayList<Passenger> = ArrayList()
+
+        @JvmStatic
+        fun initialize() {
+
+            passanger.age = 21
+            passanger.name = "Sample name"
+            passanger.passangerId = 2242
+            passanger.passengerTravelClass = 123
+            passanger.passengerTravelStatus = "Pending"
+            passanger.passengerType = "Economy"
+        }
+    }
+    @Test
+    fun `data can be added`(){
+
+        personalData.add(passanger)
+        dataCountFinal = personalData.size
+
+        assertEquals(dataCountFinal -dataCountInit,1)
+    }
+    @Test
+    fun `data can be removed`() {
+        personalData.removeAt(0)
+
+        assertEquals(personalData.size, 0)
     }
 
 }
