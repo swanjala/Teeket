9134523fa65aece62ee3dc606dff24973b1a7593
live-beta <wanjala.sam.s@gmail.com>
2019-09-03 23:11:23 +0300

    (bug): fix db init

--- src/repository/DbInit.kt
+++ src/repository/DbInit.kt
@@ -1,11 +1,37 @@
 package repository
 
+import com.mysql.cj.x.protobuf.MysqlxExpr
+import models.Passenger
 import java.sql.*
-import java.util.Properties
+import java.util.*
+import kotlin.collections.HashMap
+
 class DbInit{
     internal var connection: Connection? = null
     internal var userName = "userName"
     internal var password = "password"
+
+    fun getConnection():Connection? {
+        val connectionProps = Properties()
+        connectionProps.put("user",userName)
+        connectionProps.put("password", password)
+        try {
+            Class.forName("com.mysql.Driver").newInstance()
+            connection = DriverManager.getConnection("jdbc:" +
+                    "mysql" +
+                    "://" +
+                    "127.0.0.1" +
+                    ":" +
+                    "3306" +
+                    "/" +
+                    "",connectionProps)
+        }catch (sqlException : SQLException){
+            sqlException.printStackTrace()
+        }catch(exception:Exception){
+            exception.printStackTrace()
+        }
+        return connection
+    }
 
     fun executeMySQLQuery() {
         var statement: Statement? = null
@@ -50,25 +76,12 @@ class DbInit{
             }
         }
     }
-    fun getConnection():Connection? {
-        val connectionProps = Properties()
-        connectionProps.put("user",userName)
-        connectionProps.put("password", password)
-        try {
-            Class.forName("com.mysql.Driver").newInstance()
-            connection = DriverManager.getConnection("jdbc:" +
-                    "mysql" +
-                    "://" +
-                    "127.0.0.1" +
-                    ":" +
-                    "3306" +
-                    "/" +
-                    "",connectionProps)
-        }catch (sqlException : SQLException){
-            sqlException.printStackTrace()
-        }catch(exception:Exception){
-            exception.printStackTrace()
-        }
-        return connection
+
+    fun addData(params:HashMap<String, String>, data:Objects):Boolean{
+
+
+        "INSERT INTO "+params.get("table")+ ""
+        return false
     }
+
 }
